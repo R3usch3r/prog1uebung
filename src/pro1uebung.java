2cb5ba5a99bed10b5ab869e3360871da4bbbb3f6
R3usch3r <julian.reuscher@gmx.de>
2018-10-09 12:00:39 +0200

    init

--- src/pro1uebung.java
+++ src/pro1uebung.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class pro1uebung {
     public static void main(String[] args) {
         System.out.println("hello world");
+        blattAufgabe1();
+    }
+
+    public static void blattAufgabe1() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Zahl eingeben");
+        int eingeleseneZahl = sc.nextInt();
+        int restWert = eingeleseneZahl % 17;
+        System.out.println("Der Rest bei Division durch 17 ist" +restWert);
+
+        if (restWert == 13) {
+            System.out.println("Pechkeks!");
+        }
+
     }
 }
