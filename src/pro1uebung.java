e8b9920b26221cae99346f88bae945f6ea0032d4
R3usch3r <julian.reuscher@gmx.de>
2018-10-09 13:27:44 +0200

    init

--- src/pro1uebung.java
+++ src/pro1uebung.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class pro1uebung {
     public static void main(String[] args) {
         System.out.println("hello world");
-        blattAufgabe1();
+        blatt2Aufgabe2();
     }
 
     public static void blattAufgabe1() {
@@ -16,6 +16,50 @@ public class pro1uebung {
         if (restWert == 13) {
             System.out.println("Pechkeks!");
         }
-
     }
+
+    public static void blattAufgabe2() {
+        //Kann nicht geloest werden
+    }
+
+    public static void blatt2Aufgabe1() {
+        boolean z = true;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Booleanwert eingeben");
+        boolean x = sc.nextBoolean();
+        System.out.println("Booleanwert eingeben");
+        boolean y = sc.nextBoolean();
+
+        if((x==true) && (y == false)) {
+            z = true;
+        }
+        if((x==false) && (y == true)) {
+            z = true;
+        }
+        if((x == true) && (y == false)) {
+            z = true;
+        }
+        if((x == true) && (y==true)) {
+            z = false;
+        }
+        System.out.println(z);
+     }
+
+     public static void blatt2Aufgabe2() {
+        Scanner sc = new Scanner(System.in);
+         System.out.println("Zahl eingeben");
+         int x = sc.nextInt();
+         System.out.println("Zahl eingeben");
+         int y = sc.nextInt();
+         int summe = 0;
+
+        if(x>0 || y>0) {
+            summe = x + y;
+            System.out.println(summe);
+        }
+        if(x<0 && y<0) {
+            System.out.println(summe);
+        }
+     }
+
 }
